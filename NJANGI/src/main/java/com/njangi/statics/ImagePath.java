cd7d3eb647b3b7aef95a2fa49b366f79214d7fc3
Blanco237 <asongrandy9@gmail.com>
2022-05-20 18:36:02 +0100

    Completed User Dashboard UI

---
+++ NJANGI/src/main/java/com/njangi/statics/ImagePath.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.njangi.statics;
+
+/**
+ *
+ * @author Admin
+ */
+public interface ImagePath {
+    final String PREFIX = "C:\\Users\\Admin\\Documents\\NetBeansProjects\\NjangiRepo\\NJANGI\\src\\main\\java\\com.njangi.images\\";
+}
---
+++ NJANGI/src/main/java/com/njangi/swingcomponents/Table.java
@@ -0,0 +1,15 @@
+
+package com.njangi.swingcomponents;
+
+import javax.swing.JTable;
+
+/**
+ *
+ * @author BLANCO
+ */
+public class Table extends JTable{
+    
+    public Table(){
+        
+    }
+}
